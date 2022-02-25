d03eadef03a4224bd5b5e1f25b024020afc8c171
Diego de Santos <91721855+Diegodesantos1@users.noreply.github.com>
2022-02-25 09:58:27 +0000

    eliminada la primera función y creada la función palíndromo

--- ejercicio2.py
+++ ejercicio2.py
@@ -1,9 +1,20 @@
 print("Que palabra, frase o número quieres comprobar para ver si es un palíndromo?")
 dato=input()
-def preparacion_palabra():
-  i,j = "áéíóúñÁÉÍÓÚÑ" , "aeiounAEIOUN"
-  acento=str.maketrans(i,j)
-  dato=dato.translate(acento)
-  dato=dato.lower() #Se transforma todo en minúsculas
-  dato=dato.replace(" ", "") #Se quitan los espacios para que sea más sencillo
+
+i,j = "áéíóúñÁÉÍÓÚÑ" , "aeiounAEIOUN"
+acento=str.maketrans(i,j)
+dato=dato.translate(acento)
+dato=dato.lower() #Se transforma todo en minúsculas
+dato=dato.replace(" ", "") #Se quitan los espacios para que sea más sencillo
+lista = list(dato)
+listafinal= list(reversed(dato))
+def comprobar_palindromo():
+  if len(dato) < 1:
+    print("Es un palíndromo") #Caso para una sola letra o número
+  else:
+    if palabra[0] == palabra [-1]:
+      print(comprobar_palindromo(palabra[1:-1]))
+    else:
+      print(f"{dato} no es un palíndromo")
+comprobar_palindromo()
 
