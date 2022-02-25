03816321609016b47fc6970b1ce199c31b6d7876
Diego de Santos <91721855+Diegodesantos1@users.noreply.github.com>
2022-02-25 09:59:34 +0000

    cambiada la variable palabra, ya que en realidad es dato ya que pueden existir números

--- ejercicio2.py
+++ ejercicio2.py
@@ -12,8 +12,8 @@ def comprobar_palindromo():
   if len(dato) < 1:
     print("Es un palíndromo") #Caso para una sola letra o número
   else:
-    if palabra[0] == palabra [-1]:
-      print(comprobar_palindromo(palabra[1:-1]))
+    if dato[0] == dato [-1]:
+      print(comprobar_palindromo(dato[1:-1]))
     else:
       print(f"{dato} no es un palíndromo")
 comprobar_palindromo()
