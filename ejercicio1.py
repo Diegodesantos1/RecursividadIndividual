96b3b60b720715d56ece2643381e84ba37a6812e
Diego de Santos <91721855+Diegodesantos1@users.noreply.github.com>
2022-02-25 10:11:57 +0000

    eliminado la cota inferior 0, ya que no es necesaria y cambiado el nombre de intervalo superior a cota superior, ya que así queda más claro

--- ejercicio1.py
+++ ejercicio1.py
@@ -1,10 +1,9 @@
 import sys
 lista_palabra=["Alien","Eduardo","Cabeza","Rubén","Javier","Atlas","Río", "Servir", "Abrir","Camión", "Ordenador", "Mandaloriano", "Conflicto", "Móvil", "Estambul","Wifi", "Nft", "Real Madrid","Dicotomía","Gafas", "Pesado", "Bicicleta","Reloj", "Abrigo", "Criptoestafa", "Emblemático","Inanición","Comunicación","Ifema","Spa","Auditorio","Campo", "Bernabéu", "Iluminati", "Copiar", "Libre", "Como", "El", "Mar"]
 print(lista_palabra)
-intervalo_inf=0
-intervalo_sup=len(lista_palabra)
-numero_medio=(intervalo_sup+intervalo_inf)// 2
-print("Qué palabra quieres buscar?")
+cota_sup=len(lista_palabra)
+numero_medio=(cota_sup)// 2
+print("¿Qué palabra quieres buscar?")
 palabra_buscar=str(input())
 def dicotomia(numero_medio):
   numero_tabla =lista_palabra.index(palabra_buscar)
