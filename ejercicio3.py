369a2d69ad58498f66ea88385aa716a8d788e012
Diego de Santos <91721855+Diegodesantos1@users.noreply.github.com>
2022-02-25 13:33:16 +0000

    definida bandera final y planteada la recursividad, sigue dando errores

--- ejercicio3.py
+++ ejercicio3.py
@@ -1,18 +1,22 @@
 bandera =["R","B","B","R","G","B","B","R","B","R","R","G","R","R","B","G","G"]
 print(bandera)
-def ordenar_bandera():
+def ordenar_bandera(bandera):
   rojo=[]
   verde=[]
   azul =[]
   color=bandera.pop(0)
   if color =="R":
     rojo.append(color)
+    ordenar_bandera(bandera)
   elif color =="G":
     verde.append(color)
+    ordenar_bandera(bandera)
   elif color =="B":
     azul.append(color)
+    ordenar_bandera(bandera)
   else:
+    bandera_ordenada=rojo+verde+azul
     print(bandera_ordenada)
-ordenar_bandera()
+ordenar_bandera(bandera)
     
     
