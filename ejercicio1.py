e59010e68a0839dc8e38750711859a91af76915d
Diego de Santos <91721855+Diegodesantos1@users.noreply.github.com>
2022-02-25 08:45:46 +0000

    cambios en la estructura del código para aplicar recursividad

--- ejercicio1.py
+++ ejercicio1.py
@@ -1,22 +1,28 @@
 import sys
 def tabla():
-  lista=[]
-  if len(lista) < 20:
-    numero=random.randint(0,100)
-    lista.append(numero)
-    palabra=lista_palabras.pop()
-    
-  
-  lista.append
+  lista_palabra=["Alien","Eduardo","Cabeza", 5,"Rusia","Avión", 3,"Mascarilla","Algoritmo","Logaritmo", 12,"Integral","Naranja","Martín","Juan","Leonardo","Javier","Estados Unidos"]
+  print(lista_palabra)
+tabla()
+
+print("Establece el intervalo inferior de búsqueda")
+intervalo_inf=int(input())
+print("Establece el intervalo superior de búsqueda")
+intervalo_sup=int(input())
+if intervalo_inf >= intervalo_sup:
+  print("No se puede realizar la búsqueda")
+  sys.exit
+else:
+  print("Qué palabra quieres buscar?")
+  palabra_buscar=str(input())
+  numero_medio= intervalo_sup//intervalo_inf
 def dicotomia():
-  print("Establece el intervalo inferior de búsqueda")
-  intervalo_inf=int(input())
-  print("Establece el intervalo superior de búsqueda")
-  intervalo_sup=int(input())
-  if intervalo_inf <= intervalo_sup:
-    print("No se puede realizar la búsqueda")
-    dicotomia()
+  numero_medio =-1
+  if lista_palabras[numero_medio] == palabra:
+    print(f"La palabra es {palabra_buscar}")
   else:
+    dicotomia()
+    
+dicotomia()
     
     
     
