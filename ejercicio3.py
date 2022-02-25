0a5d9491c4e9698b4c69c61cbe481f6bdb396cda
Diego de Santos <91721855+Diegodesantos1@users.noreply.github.com>
2022-02-25 13:26:27 +0000

    borrado y vuelta a empezar

--- ejercicio3.py
+++ ejercicio3.py
@@ -4,24 +4,7 @@ def ordenar_bandera():
   rojo=[]
   verde=[]
   azul =[]
-  if bandera.index("R"):
-    color=bandera.pop(1)
-    rojo.append(color)
-    print(rojo)
-    ordenar_bandera()
-  elif bandera.index("G"):
-    color=bandera.pop(1)
-    verde.append(color)
-    print(verde)
-    ordenar_bandera()
-  elif bandera.index("B"):
-    color=bandera.pop(1)
-    azul.append(color)
-    print(azul)
-    ordenar_bandera()
-  else:
-    bandera_ordenada=(rojo + verde + azul)
-    print(bandera_ordenada)
+
 ordenar_bandera()
     
     
