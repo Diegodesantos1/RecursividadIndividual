195ef570f4c4abb57a9a6beb952ffd0229ab9ead
Diego de Santos <91721855+Diegodesantos1@users.noreply.github.com>
2022-02-25 13:38:09 +0000

    solucionado el error creando un if que compruebe que la longitud de la tabla sea superior a 0 para que se ejecute la función recursiva

--- ejercicio3.py
+++ ejercicio3.py
@@ -4,19 +4,23 @@ rojo=[]
 verde=[]
 azul =[]
 def ordenar_bandera(bandera):
-  color=bandera.pop(0)
-  if color =="R":
-    rojo.append(color)
-    ordenar_bandera(bandera)
-  elif color =="G":
-    verde.append(color)
-    ordenar_bandera(bandera)
-  elif color =="B":
-    azul.append(color)
-    ordenar_bandera(bandera)
+  if len(bandera) > 0:
+    color=bandera.pop(0)
+    if color =="R":
+      rojo.append(color)
+      ordenar_bandera(bandera)
+    elif color =="G":
+      verde.append(color)
+      ordenar_bandera(bandera)
+    elif color =="B":
+      azul.append(color)
+      ordenar_bandera(bandera)
+    else:
+      bandera_ordenada=rojo+verde+azul
+      print(bandera_ordenada)
   else:
-    bandera_ordenada=rojo+verde+azul
-    print(bandera_ordenada)
+      bandera_ordenada=rojo+verde+azul
+      print(bandera_ordenada)
 ordenar_bandera(bandera)
     
     
