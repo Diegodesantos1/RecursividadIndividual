39438f749683feaf04f30bf635ec67895de5c6ca
Diego de Santos <91721855+Diegodesantos1@users.noreply.github.com>
2022-02-25 09:30:05 +0000

    Creada la recursividad tanto en el intervalo mayor como inferior

--- ejercicio1.py
+++ ejercicio1.py
@@ -5,17 +5,17 @@ print(lista_palabra)
 intervalo_inf=0
 intervalo_sup=len(lista_palabra)
 numero_medio=(intervalo_sup+intervalo_inf)// 2
-print(numero_medio)
 print("Qué palabra quieres buscar?")
 palabra_buscar=str(input())
-def dicotomia():
+def dicotomia(numero_medio):
   numero_tabla =lista_palabra.index(palabra_buscar)
   if numero_tabla == numero_medio:
-    print(f"La palabra es {palabra_buscar}")
+    print(f"La palabra {palabra_buscar} se encuentra en el índice {numero_tabla}")
   elif numero_tabla < numero_medio:
-    
-
-dicotomia()
+    dicotomia(numero_medio-1)
+  elif numero_tabla > numero_medio:
+    dicotomia(numero_medio+1)
+dicotomia(numero_medio)
     
     
     
