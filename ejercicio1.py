7f4bbb3c485e6d02e6b03a4ef185169a5d1b9d17
Diego de Santos <91721855+Diegodesantos1@users.noreply.github.com>
2022-02-25 08:56:21 +0000

    todavia no funciona la funcion dicotomia

--- ejercicio1.py
+++ ejercicio1.py
@@ -1,27 +1,26 @@
 import sys
-def tabla():
-  lista_palabra=["Alien","Eduardo","Cabeza", 5,"Rusia","Avión", 3,"Mascarilla","Algoritmo","Logaritmo", 12,"Integral","Naranja","Martín","Juan","Leonardo","Javier","Estados Unidos"]
-  print(lista_palabra)
-tabla()
 
+lista_palabra=["Alien","Eduardo","Cabeza"]
+print(lista_palabra)
 print("Establece el intervalo inferior de búsqueda")
 intervalo_inf=int(input())
 print("Establece el intervalo superior de búsqueda")
 intervalo_sup=int(input())
-if intervalo_inf >= intervalo_sup:
+if intervalo_inf > intervalo_sup or intervalo_inf == 0:
   print("No se puede realizar la búsqueda")
   sys.exit
 else:
   print("Qué palabra quieres buscar?")
   palabra_buscar=str(input())
   numero_medio= intervalo_sup//intervalo_inf
-def dicotomia():
-  numero_medio =-1
-  if lista_palabras[numero_medio] == palabra:
-    print(f"La palabra es {palabra_buscar}")
-  else:
-    dicotomia()
-    
+  def dicotomia():
+    numero_medio =-1
+    palabra_tabla =lista_palabra.index("palabra_buscar")
+    if palabra_tabla == palabra_buscar:
+      print(f"La palabra es {palabra_buscar}")
+    else:
+      dicotomia()
+
 dicotomia()
     
     
