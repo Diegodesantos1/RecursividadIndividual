b3a0817fd9d4376f322db14a5a9378e1cd8918d2
Diego de Santos <91721855+Diegodesantos1@users.noreply.github.com>
2022-02-25 14:18:48 +0000

    Puesta la función en función de bandera

--- ejercicio3.py
+++ ejercicio3.py
@@ -3,24 +3,24 @@ print(bandera)
 rojo=[]
 verde=[]
 azul =[]
-def ordenar_bandera():
+def ordenar_bandera(bandera):
   if len(bandera) > 0:
     color=bandera.pop(0)
     if color =="R":
       rojo.append(color)
-      ordenar_bandera()
+      ordenar_bandera(bandera)
     elif color =="G":
       verde.append(color)
-      ordenar_bandera()
+      ordenar_bandera(bandera)
     elif color =="B":
       azul.append(color)
-      ordenar_bandera()
+      ordenar_bandera(bandera)
     else:
       bandera_ordenada=rojo+verde+azul
       print(f"La bandera ordenada es {bandera_ordenada}")
   else:
       bandera_ordenada=rojo+verde+azul
       print(f"La bandera ordenada es {bandera_ordenada}")
-ordenar_bandera()
+ordenar_bandera(bandera)
     
     
