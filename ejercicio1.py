b9050d785bb1583b8559076ac002dd2024d4efd8
Diego de Santos <91721855+Diegodesantos1@users.noreply.github.com>
2022-02-25 08:05:22 +0000

    Creados los ejercicios, el main, y planteado la función del primer ejercicio y aplicando recursividad en el if por ahora

---
+++ ejercicio1.py
@@ -0,0 +1,15 @@
+import sys
+def tabla():
+  
+def dicotomia():
+  print("Establece el intervalo inferior de búsqueda")
+  intervalo_inf=int(input())
+  print("Establece el intervalo superior de búsqueda")
+  intervalo_sup=int(input())
+  if intervalo_inf <= intervalo_sup:
+    print("No se puede realizar la búsqueda")
+    dicotomia()
+  else:
+    
+    
+    
