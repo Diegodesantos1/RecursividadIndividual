b7a3047029a4f6facda2720b3f92c8aad9c65788
Diego de Santos <91721855+Diegodesantos1@users.noreply.github.com>
2022-02-25 13:19:10 +0000

    Creadas las listas separadas de cada color, falta juntarlas y arreglar errores

--- ejercicio3.py
+++ ejercicio3.py
@@ -1,1 +1,28 @@
-Bandera =["R","B","B","R","G","B","B","R","B","R","R","G","R","R","B","G","G"]
+bandera =["R","B","B","R","G","B","B","R","B","R","R","G","R","R","B","G","G"]
+print(bandera)
+def ordenar_bandera():
+  rojo=[]
+  verde=[]
+  azul =[]
+  if bandera.index("R"):
+    color=bandera.pop(1)
+    rojo.append(color)
+    print(rojo)
+    ordenar_bandera()
+  elif bandera.index("G"):
+    color=bandera.pop(1)
+    verde.append(color)
+    print(verde)
+    ordenar_bandera()
+  elif bandera.index("B"):
+    color=bandera.pop(1)
+    azul.append(color)
+    print(azul)
+    ordenar_bandera()
+  print(bandera)
+  print(rojo)
+  print(verde)
+  print(azul)
+ordenar_bandera()
+    
+    
